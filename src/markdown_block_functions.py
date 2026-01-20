1af516a198bdbc840dad8aee11b8177689324a42
Eduardo Paul <ecpaul@pm.me>
2026-01-20 15:21:25 -0300

    Add block type identification functions and tests.

--- src/markdown_block_functions.py
+++ src/markdown_block_functions.py
@@ -1,3 +1,15 @@
+from enum import Enum
+from re import match, search
+
+
+class BlockType(Enum):
+    PARAGRAPH = "paragraph"
+    HEADING = "heading"
+    CODE = "code"
+    QUOTE = "quote"
+    UNORDERED_LIST = "unordered_list"
+    ORDERED_LIST = "ordered_list"
+
 
 def markdown_to_blocks(markdown):
     list_of_blocks = markdown.split("\n\n")
@@ -7,4 +19,39 @@ def markdown_to_blocks(markdown):
     ]
 
     return new_markdown
+
+
+def block_to_block_type(block):
+
+    if match(r"#{1,6}", block):
+        block_type = BlockType.HEADING
+    elif match(r"```\n", block) and search(r"```$", block):
+        block_type = BlockType.CODE
+    elif all(match(r"> ", line) for line in block.splitlines()):
+        block_type = BlockType.QUOTE
+    elif all(match(r"- ", line) for line in block.splitlines()):
+        block_type = BlockType.UNORDERED_LIST
+    elif test_ordered_list(block):
+        block_type = BlockType.ORDERED_LIST
+    else:
+        block_type = BlockType.PARAGRAPH
+
+    return block_type
+
+
+def test_ordered_list(block):
+    try:
+        first_idx = match(r"(\d+)\.", block).group(1)
+    except AttributeError:
+        return False
+
+    idx = int(first_idx)
+
+    lines = block.splitlines()
+    for line in lines:
+        if not match(f"{idx}\\.", line):
+            return False
+        idx += 1
+
+    return True
 
---
+++ src/test_markdown_block_functions.py
@@ -0,0 +1,202 @@
+from textwrap import dedent
+import unittest
+
+from markdown_block_functions import BlockType, block_to_block_type, markdown_to_blocks
+
+
+class TestMarkdownToBlocks(unittest.TestCase):
+    def test_simple_case(self):
+
+        md = dedent("""
+            This is **bolded** paragraph
+
+            This is another paragraph with _italic_ text and `code` here
+            This is the same paragraph on a new line
+
+            - This is a list
+            - with items
+        """)
+
+        blocks = markdown_to_blocks(md)
+
+        self.assertEqual(
+            blocks,
+            [
+                "This is **bolded** paragraph",
+                "This is another paragraph with _italic_ text and `code` here\nThis is the same paragraph on a new line",
+                "- This is a list\n- with items",
+            ],
+        )
+
+
+class TestBlockToBlockType(unittest.TestCase):
+    def test_normal_paragraph(self):
+        block = "This is just a normal paragraph."
+
+        block_type = BlockType.PARAGRAPH
+
+        self.assertEqual(
+            block_to_block_type(block),
+            block_type,
+        )
+
+    def test_heading_1(self):
+        block = "# This is a first level heading."
+
+        block_type = BlockType.HEADING
+
+        self.assertEqual(
+            block_to_block_type(block),
+            block_type,
+        )
+
+    def test_heading_2(self):
+        block = "## This is a second level heading."
+
+        block_type = BlockType.HEADING
+
+        self.assertEqual(
+            block_to_block_type(block),
+            block_type,
+        )
+
+    def test_code(self):
+        block = dedent("""
+            ```
+            for file in *; do
+                echo "$file"
+            done
+            ```
+       """).strip()
+
+        block_type = BlockType.CODE
+
+        self.assertEqual(
+            block_to_block_type(block),
+            block_type,
+        )
+
+    def test_quote(self):
+        block = dedent("""
+            > Beautiful quote from a random book.
+            > Another one.
+        """).strip()
+
+        block_type = BlockType.QUOTE
+
+        self.assertEqual(
+            block_to_block_type(block),
+            block_type,
+        )
+
+    def test_quote_fail(self):
+        block = dedent("""
+            > Beautiful quote from a random book.
+            Oops.
+        """).strip()
+
+        block_type = BlockType.QUOTE
+
+        self.assertNotEqual(
+            block_to_block_type(block),
+            block_type,
+        )
+
+    def test_unordered_list(self):
+        block = dedent("""
+            - First item of a very interesting list.
+            - Second item.
+        """).strip()
+
+        block_type = BlockType.UNORDERED_LIST
+
+        self.assertEqual(
+            block_to_block_type(block),
+            block_type,
+        )
+
+    def test_unordered_list_fail(self):
+        block = dedent("""
+            - First item of a very interesting list.
+            Oops.
+        """).strip()
+
+        block_type = BlockType.PARAGRAPH
+
+        self.assertEqual(
+            block_to_block_type(block),
+            block_type,
+        )
+
+    def test_ordered_list_one_item(self):
+        block = dedent("""
+            1. First item of a very interesting list.
+        """).strip()
+
+        block_type = BlockType.ORDERED_LIST
+
+        self.assertEqual(
+            block_to_block_type(block),
+            block_type,
+        )
+
+    def test_ordered_list_multiple_items(self):
+        block = dedent("""
+            1. First item of a very interesting list.
+            2. Second item of a very interesting list.
+            3. Third item of a very interesting list.
+        """).strip()
+
+        block_type = BlockType.ORDERED_LIST
+
+        self.assertEqual(
+            block_to_block_type(block),
+            block_type,
+        )
+
+    def test_ordered_list_multiple_items_out_of_order(self):
+        block = dedent("""
+            1. First item of a very interesting list.
+            3. Second item of a very interesting list.
+            2. Third item of a very interesting list.
+        """).strip()
+
+        block_type = BlockType.PARAGRAPH
+
+        self.assertEqual(
+            block_to_block_type(block),
+            block_type,
+        )
+
+    def test_ordered_list_multiple_items_random_start(self):
+        block = dedent("""
+            4. First item of a very interesting list.
+            5. Second item of a very interesting list.
+            6. Third item of a very interesting list.
+        """).strip()
+
+        block_type = BlockType.ORDERED_LIST
+
+        self.assertEqual(
+            block_to_block_type(block),
+            block_type,
+        )
+
+    def test_ordered_list_false(self):
+        block = dedent("""
+            First item of a malformed interesting list.
+            5. Second item of a very interesting list.
+            6. Third item of a very interesting list.
+        """).strip()
+
+        block_type = BlockType.PARAGRAPH
+
+        self.assertEqual(
+            block_to_block_type(block),
+            block_type,
+        )
+
+
+if __name__ == "__main__":
+    unittest.main()
+
