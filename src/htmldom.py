c5c7316a47db586b9a83bf1aeded4bb69649cf5b
Eduardo Paul <ecpaul@pm.me>
2026-02-24 15:41:23 -0300

    Updated the `Node` class to resemble the HTML DOM standard.

--- src/htmldom.py
+++ src/htmldom.py
@@ -13,7 +13,100 @@ class Tag(Enum):
 
 
 class Node:
-    pass
+    def __init__(self):
+        self.parentNode = None
+        self.parentElement = None
+
+        self.childNodes = []
+        self.firstChild = None
+        self.lastChild = None
+
+        self.previousSibling = None
+        self.nextSibling = None
+
+    def _update_first_and_last_children(self):
+        if self.childNodes:
+            self.firstChild = self.childNodes[0]
+            self.lastChild = self.childNodes[-1]
+        else:
+            self.firstChild = None
+            self.lastChild = None
+
+    def _set_parentNode(self, parentNode):
+        self.parentNode = parentNode
+
+        if isinstance(parentNode, Element):
+            self.parentElement = parentNode
+
+        idx = parentNode.childNodes.index(self)
+        if idx > 0:
+            self.previousSibling = parentNode.childNodes[idx-1]
+        if idx < len(parentNode.childNodes) - 1:
+            self.nextSibling = parentNode.childNodes[idx+1]
+
+    def _unset_parentNode(self):
+        self.parentNode = None
+        self.parentElement = None
+        self.previousSibling = None
+        self.nextSibling = None
+
+    def appendChild(self, childNode):
+        if not isinstance(childNode, Node):
+            raise TypeError(f"The parameter `childNode` must be a `Node`, not `{type(childNode).__name__}`")
+
+        if childNode.parentNode is not None:
+            childNode.parentNode.removeChild(childNode)
+
+        self.childNodes.append(childNode)
+        childNode._set_parentNode(self)
+        self._update_first_and_last_children()
+
+    def removeChild(self, childNode):
+        if not isinstance(childNode, Node):
+            raise TypeError(f"The parameter `childNode` must be a `Node`, not `{type(childNode).__name__}`")
+
+        if childNode not in self.childNodes:
+            return
+
+        self.childNodes.remove(childNode)
+        childNode._unset_parentNode()
+        self._update_first_and_last_children()
+
+    def insertBefore(self, newChildNode, referenceChildNode):
+        if not isinstance(newChildNode, Node):
+            raise TypeError(f"The parameter `newChildNode` must be a `Node`, not `{type(newChildNode).__name__}`")
+
+        if not isinstance(referenceChildNode, Node):
+            raise TypeError(f"The parameter `referenceChildNode` must be a `Node`, not `{type(referenceChildNode).__name__}`")
+
+        if referenceChildNode not in self.childNodes:
+            raise ValueError("The given `referenceChildNode` is not one of current the `Node`'s children.")
+
+        if newChildNode.parentNode is not None:
+            newChildNode.parentNode.removeChild(newChildNode)
+
+        idx = self.childNodes.index(referenceChildNode)
+        self.childNodes.insert(idx, newChildNode)
+        self._update_first_and_last_children()
+
+        newChildNode._set_parentNode(self)
+
+    def replaceChild(self, newChildNode, oldChildNode):
+        if not isinstance(newChildNode, Node):
+            raise TypeError(f"The parameter `newChildNode` must be a `Node`, not `{type(newChildNode).__name__}`")
+
+        if not isinstance(oldChildNode, Node):
+            raise TypeError(f"The parameter `oldChildNode` must be a `Node`, not `{type(oldChildNode).__name__}`")
+
+        if oldChildNode not in self.childNodes:
+            raise ValueError("The given `oldChildNode` is not one of current the `Node`'s children.")
+
+        idx = self.childNodes.index(oldChildNode)
+        self.childNodes[idx] = newChildNode
+        self._update_first_and_last_children()
+
+        newChildNode._set_parentNode(self)
+        oldChildNode._unset_parentNode()
 
 
 class ElementNode(Node):
