d14f7029d5157962a3fc5b14efc8207bb320bdce
Eduardo Paul <ecpaul@pm.me>
2026-05-08 17:52:45 -0300

    Started implementing `markdown_to_html_node` function. Got simple text and text with inline elements working.

--- src/markdown_to_html.py
+++ src/markdown_to_html.py
@@ -1,12 +1,12 @@
-from htmlnode import LeafNode
+from htmlnode import HTMLNode
 import markdown_functions
-from markdown_block_functions import markdown_to_blocks, block_to_block_type
 from textnode import TextNode, TextType
+from text_to_html import text_node_to_html_node
 
 
 def markdown_to_textnodes(markdown):
     input_textnode = TextNode(markdown, TextType.PLAIN)
-    textnodes =  markdown_functions.split_nodes_delimiter([input_textnode], "**", TextType.BOLD)
+    textnodes = markdown_functions.split_nodes_delimiter([input_textnode], "**", TextType.BOLD)
     textnodes = markdown_functions.split_nodes_delimiter(textnodes, "_", TextType.ITALIC)
     textnodes = markdown_functions.split_nodes_delimiter(textnodes, "`", TextType.CODE)
     textnodes = markdown_functions.split_nodes_image(textnodes)
@@ -15,43 +15,16 @@ def markdown_to_textnodes(markdown):
     return textnodes
 
 
-def textnodes_to_leafnodes(textnodes):
-    leafnodes = []
-    for textnode in textnodes:
-        text = textnode.text
-        tag = get_tag_from_texttype(textnode.text_type)
-        props = textnode.props
+def markdown_to_html_node(markdown: str) -> HTMLNode:
+    """Consume a markdown document and produce from it a single `HTMLNode`. The output node should generate the expected html code, being a standalone `div` element."""
 
-        leafnodes.append(
-            LeafNode(
-                tag=tag,
-                value=text,
-                props=props,
-            )
+    html_node = HTMLNode(tag="div", children=[])
+
+    textnodes = markdown_to_textnodes(markdown)
+    for textnode in textnodes:
+        html_node.children.append(
+            text_node_to_html_node(textnode)
         )
 
-    return leafnodes
-
-
-def get_tag_from_texttype(texttype):
-    match texttype:
-        case TextType.PLAIN:
-            tag = None
-        case TextType.BOLD:
-            tag = 'b'
-        case TextType.ITALIC:
-            tag = 'i'
-        case TextType.CODE:
-            tag = 'code'
-        case TextType.LINK:
-            tag = 'a'
-        case TextType.IMAGE:
-            tag = 'img'
-
-    return tag
-
-
-def markdown_to_html_node(markdown: str) -> HTMLNode:
-    """Consume a markdown page and produce from it its equivalent as an HTMLNode ready to be displayed as a web page."""
-    return HTMLNode()
+    return html_node
 
--- tests/test_markdown_to_html.py
+++ tests/test_markdown_to_html.py
@@ -1,7 +1,7 @@
 import unittest
 
-from htmlnode import LeafNode
-from markdown_to_html import markdown_to_textnodes, textnodes_to_leafnodes
+from htmlnode import HTMLNode, LeafNode
+from markdown_to_html import markdown_to_textnodes, markdown_to_html_node
 from textnode import TextNode, TextType
 
 
@@ -42,40 +42,24 @@ class TestsTextToTextNodes(unittest.TestCase):
         )
 
 
-class TestTextnodesToLeafnodes(unittest.TestCase):
-    def test_leaves_with_simple_delimiters(self):
-        textnodes = [
-            TextNode("This is text with an ", TextType.PLAIN),
-            TextNode("italic", TextType.ITALIC),
-            TextNode(" and a ", TextType.PLAIN),
-            TextNode("bold", TextType.BOLD),
-            TextNode(" word. We also have a small ", TextType.PLAIN),
-            TextNode("code block", TextType.CODE),
-            TextNode(" and a ", TextType.PLAIN),
-            TextNode("link", TextType.LINK, {"href": "example.com"}),
-            TextNode(None, TextType.IMAGE, {"alt": "alt text", "src": "image.png"}),
+class TestMarkdownToHtmlNode(unittest.TestCase):
+    def test_single_line_of_text(self):
+        markdown = "simple line of text"
+        html = "<div>simple line of text</div>"
 
-        ]
-
-        template = [
-            LeafNode(value="This is text with an ", tag=None),
-            LeafNode(value="italic", tag="i"),
-            LeafNode(value=" and a ", tag=None),
-            LeafNode(value="bold", tag="b"),
-            LeafNode(value=" word. We also have a small ", tag=None),
-            LeafNode(value="code block", tag="code"),
-            LeafNode(value=" and a ", tag=None),
-            LeafNode(value="link", tag="a", props={"href": "example.com"}),
-            LeafNode(value=None, tag="img", props={"alt": "alt text", "src": "image.png"}),
-        ]
-
-        leafnodes = textnodes_to_leafnodes(textnodes)
-
-        self.assertListEqual(
-                leafnodes,
-                template,
+        self.assertEqual(
+            markdown_to_html_node(markdown).to_html(),
+            html,
         )
 
+    def test_single_line_with_structure(self):
+        markdown = "This is a **test case** with _structure_, an image (![image](www.example.com)) and a [link](www.example.com). Also some `code`."
+        html = '<div>This is a <b>test case</b> with <i>structure</i>, an image (<img alt="image" src="www.example.com">) and a <a href="www.example.com">link</a>. Also some <code>code</code>.</div>'
+
+        self.assertEqual(
+            markdown_to_html_node(markdown).to_html(),
+            html,
+        )
 
 if __name__ == "__main__":
     unittest.main()
